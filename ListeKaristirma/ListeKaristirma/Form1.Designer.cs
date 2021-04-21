cbb17b67be21b7fa6a6b068021ed5241aa3375c0
Sadigzade Mikayil <75858136+mikayil1551@users.noreply.github.com>
2021-04-21 12:41:16 +0300

    Add files via upload

---
+++ ListeKaristirma/ListeKaristirma/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace ListeKaristirma
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.btnKaristir = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "İsim";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(76, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.BackColor = System.Drawing.SystemColors.Window;
+            this.btnEkle.Location = new System.Drawing.Point(232, 25);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(75, 23);
+            this.btnEkle.TabIndex = 2;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = false;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            this.btnEkle.MouseLeave += new System.EventHandler(this.btnEkle_MouseLeave);
+            this.btnEkle.MouseHover += new System.EventHandler(this.btnEkle_MouseHover);
+            // 
+            // btnSil
+            // 
+            this.btnSil.BackColor = System.Drawing.SystemColors.Window;
+            this.btnSil.Location = new System.Drawing.Point(232, 61);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(75, 23);
+            this.btnSil.TabIndex = 3;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = false;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            this.btnSil.MouseLeave += new System.EventHandler(this.btnSil_MouseLeave);
+            this.btnSil.MouseHover += new System.EventHandler(this.btnSil_MouseHover);
+            // 
+            // btnKaristir
+            // 
+            this.btnKaristir.BackColor = System.Drawing.SystemColors.Window;
+            this.btnKaristir.Location = new System.Drawing.Point(232, 106);
+            this.btnKaristir.Name = "btnKaristir";
+            this.btnKaristir.Size = new System.Drawing.Size(75, 23);
+            this.btnKaristir.TabIndex = 4;
+            this.btnKaristir.Text = "Karistir";
+            this.btnKaristir.UseVisualStyleBackColor = false;
+            this.btnKaristir.Click += new System.EventHandler(this.btnKaristir_Click);
+            this.btnKaristir.MouseLeave += new System.EventHandler(this.btnKaristir_MouseLeave);
+            this.btnKaristir.MouseHover += new System.EventHandler(this.btnKaristir_MouseHover);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(16, 70);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.listBox1.Size = new System.Drawing.Size(160, 134);
+            this.listBox1.TabIndex = 5;
+            this.listBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.listBox1_KeyDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnKaristir);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Button btnKaristir;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ ListeKaristirma/ListeKaristirma/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListeKaristirma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Trim()!=string.Empty)
+            {
+                string isim = string.Format("{0}-{1}",listBox1.Items.Count+1,textBox1.Text.Trim());
+          
+                if (listBox1.Items.Contains(isim) == true)
+                {
+                    MessageBox.Show("Isim girilmisdir.Lutfen baska isim giriniz:)");
+                    textBox1.Clear();
+                    textBox1.Focus();
+                }
+                else
+                {
+                    listBox1.Items.Add(isim);
+                    textBox1.Clear();
+                    textBox1.Focus();
+                }
+            }
+           
+            
+        }
+
+        private void btnKaristir_Click(object sender, EventArgs e)
+        {
+            string[] isimler = new string[listBox1.Items.Count];
+            Random rnd = new Random();
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                int sayi = rnd.Next(0, listBox1.Items.Count);
+                string rastgele = listBox1.Items[sayi].ToString();
+
+                if (isimler.Contains(rastgele)==false)
+                {
+                    isimler[i] = rastgele;
+
+                }
+                else
+                {
+                    i--;
+                }
+            }
+            listBox1.Items.Clear();
+            foreach (string item in isimler)
+            {
+                listBox1.Items.Add(item);
+
+            }
+
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            DialogResult sonuc =  MessageBox.Show("Silmek istediginizden emin misiniz", "Silme Formu",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question);
+            if (sonuc==DialogResult.Yes)
+            {
+                string[] silinecekler = new string[listBox1.SelectedItems.Count];
+                for (int i = 0; i < listBox1.SelectedItems.Count; i++)
+                {
+                    silinecekler[i] = (string)listBox1.SelectedItems[i];
+                }
+                foreach (string item in silinecekler)
+                {
+                    listBox1.Items.Remove(item);
+
+                }
+            }
+           
+            
+        }
+
+        private void btnEkle_MouseHover(object sender, EventArgs e)
+        {
+            btnEkle.BackColor = Color.Red;
+        }
+
+        private void btnEkle_MouseLeave(object sender, EventArgs e)
+        {
+            btnEkle.BackColor = Color.White;
+        }
+
+        private void btnSil_MouseHover(object sender, EventArgs e)
+        {
+            btnSil.BackColor = Color.Red;
+        }
+
+        private void btnSil_MouseLeave(object sender, EventArgs e)
+        {
+            btnSil.BackColor = Color.White;
+        }
+
+        private void btnKaristir_MouseHover(object sender, EventArgs e)
+        {
+            btnKaristir.BackColor = Color.Red;
+        }
+
+        private void btnKaristir_MouseLeave(object sender, EventArgs e)
+        {
+            btnKaristir.BackColor = Color.White;
+        }
+
+        private void listBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            DialogResult sonuc = MessageBox.Show("Silmek istediginizden emin misiniz", "Silme Formu",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question);
+            if (sonuc == DialogResult.Yes)
+            {
+                string[] silinecekler = new string[listBox1.SelectedItems.Count];
+                for (int i = 0; i < listBox1.SelectedItems.Count; i++)
+                {
+                    silinecekler[i] = (string)listBox1.SelectedItems[i];
+                }
+                foreach (string item in silinecekler)
+                {
+                    listBox1.Items.Remove(item);
+
+                }
+            }
+        }
+    }
+}
